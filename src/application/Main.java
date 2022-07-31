a28e106d9c1de7a4128b9d004045207575cdb1b7
Abdullah Mehedi <64689725+000mehedi@users.noreply.github.com>
2022-07-31 11:56:40 -0600

    made changes to main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,16 +1,22 @@
 package application;
 	
+import java.io.FileInputStream;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			FXMLLoader loader = new FXMLLoader();
+			VBox root = loader.load(new FileInputStream("src/Main.fxml"));
+	
 			Scene scene = new Scene(root,800,800);
 			
 			primaryStage.setScene(scene);
