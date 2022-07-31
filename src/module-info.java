fb475656bfa35bd571606793f66ab96e01605200
Abdullah Mehedi <64689725+000mehedi@users.noreply.github.com>
2022-07-31 12:13:31 -0600

    created controller

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module CPSC_233_project_G54 {
 	requires javafx.controls;
 	requires javafx.graphics;
 	requires javafx.fxml;
+	requires java.desktop;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
