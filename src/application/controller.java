af007107ee53e749434aa48d17e297b504b8966d
Abdullah Mehedi <64689725+000mehedi@users.noreply.github.com>
2022-07-31 14:04:30 -0600

    .

--- src/application/controller.java
+++ src/application/controller.java
@@ -1,12 +1,26 @@
 package application;
 
 import java.awt.event.ActionEvent;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class controller{
 	@FXML
-	void buttonGetStarted(ActionEvent event) {
+	void buttonGetStarted(ActionEvent event) throws IOException{
+		VBox root = FXMLLoader.load(getClass().getResource("src/secondScreen/secondScreen.fxml"));
+		Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+		
 		
 	}
 }
---
+++ src/secondScreen/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author mdabdullah.patwary
+ *
+ */
+package secondScreen;
