0df46efc796f50fdd14377f5ba22ef3047e124c5
Abdullah Mehedi <abdullahmehedi000@gmail.com>
2022-08-04 17:04:05 -0600

    Changes to transfer.fxml

--- src/application/transferFundsController.java
+++ src/application/transferFundsController.java
@@ -1,5 +1,6 @@
 package application;
 
+<<<<<<< Updated upstream
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -36,3 +37,17 @@ public class transferFundsController implements Initializable   {
 	
 	
 
+=======
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
+public class transferFundsController {
+
+    @FXML
+    private ChoiceBox<?> toDropdown;
+
+    @FXML
+    private ChoiceBox<?> fromDropdown;
+
+}
+>>>>>>> Stashed changes
