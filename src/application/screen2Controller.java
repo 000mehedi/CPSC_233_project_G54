2fc4147b253f2936787b2dc7e9bf1e25a538e91d
Abdullah Mehedi <mdabdullah.patwary@ucalgary.ca>
2022-08-03 19:56:46 -0600

    made screen2 and 3

---
+++ src/application/screen2Controller.java
@@ -0,0 +1,18 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class screen2Controller {
+
+    @FXML
+    void createSavingsAccount(ActionEvent event) {
+
+    }
+
+    @FXML
+    void createChequingAccount(ActionEvent event) {
+
+    }
+
+}
--- src/secondScreen/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author mdabdullah.patwary
- *
- */
-package secondScreen;
