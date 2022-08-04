f01edca39dee3061783da3fe1aa677f2dd0d27d7
Abdullah Mehedi <mdabdullah.patwary@ucalgary.ca>
2022-08-03 23:13:57 -0600

    Update mainController.java

--- src/application/mainController.java
+++ src/application/mainController.java
@@ -1,13 +1,34 @@
 package application;
 
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
 public class mainController {
+	private Stage stage;
+	private Scene scene;
 
     @FXML
-    void createNewAccount(ActionEvent event) {
-
+    public void createNewAccount(ActionEvent event) throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		VBox root = loader.load(new FileInputStream("src/screens/screen2.fxml"));
+    	stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    	scene = new Scene(root);
+    	stage.setScene(scene);
+    	stage.show();
+    	
+		
     }
 
     @FXML
