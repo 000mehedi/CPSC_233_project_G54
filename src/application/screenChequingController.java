86c9c95a9466edcb617850675239df6e78fb2913
Mahira Zabin <mahirazabin@gmail.com>
2022-08-04 14:12:24 -0600

    Show User's Account Number

--- src/application/screenChequingController.java
+++ src/application/screenChequingController.java
@@ -4,14 +4,19 @@ import java.util.Random;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class screenChequingController {
+	
+	@FXML
+	private Label chequingAccNumLabel;
 
     @FXML
     void generateAccountNumber(ActionEvent event) {
     	Random random = new Random();
-    	int chequingAccountNumber = random.nextInt(10000); 
+    	double chequingAccountNumber = random.nextInt(10000); 
     	
+    	chequingAccNumLabel.setText(String.format(" Account Number : %.0f", chequingAccountNumber ));
 
     }
 
