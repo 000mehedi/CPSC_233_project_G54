08ce4bded55a60cd3e57202c4450d02f16ca8c9f
Mahira Zabin <92553688+mahirazabin@users.noreply.github.com>
2022-08-06 14:50:45 -0600

    Created Choicebox and added it to Controller

--- src/application/depositFundsController.java
+++ src/application/depositFundsController.java
@@ -1,6 +1,29 @@
 package application;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class depositFundsController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 
+public class depositFundsController implements Initializable {
+	
+	
+	@FXML
+	private Label myLabel;
+	
+	@FXML
+	private ChoiceBox<String> accountChoiceBox;
+	
+	private String[] accountType = {"Chequing Account", "Savings Account"};
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+		accountChoiceBox.getItems().addAll(accountType);
+		
+	}
+	
 }
