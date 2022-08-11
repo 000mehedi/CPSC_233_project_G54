143aca22408525a1f9835ee915d74bbb8d47fcb5
Mahira Zabin <92553688+mahirazabin@users.noreply.github.com>
2022-08-10 23:52:23 -0600

    Squashed commit of the following:
    commit 787c0afda272d8f49c7eb61ea754f3511fd931fb
    Author: Mahira Zabin <92553688+mahirazabin@users.noreply.github.com>
    Date:   Wed Aug 10 23:51:54 2022 -0600
    
        added Bank Class and more buttons

--- src/application/transferFundsController.java
+++ src/application/transferFundsController.java
@@ -1,14 +1,39 @@
 package application;
 
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class transferFundsController implements Initializable   {
+	
+	
+	private Stage stage;
+	private Scene scene;
+	
+    @FXML
+    public void backToAccountScreen(ActionEvent event) throws IOException {
+    	FXMLLoader loader = new FXMLLoader();
+		VBox root = loader.load(new FileInputStream("src/screens/screen1.fxml"));
+    	stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    	scene = new Scene(root);
+    	stage.setScene(scene);
+    	stage.show();
+    	
+    	
+
+    }
 
     @FXML
     private ChoiceBox<String> toDropdown;
