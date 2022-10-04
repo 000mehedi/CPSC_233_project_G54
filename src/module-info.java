4dcede5f3791a684728599be6dde961d256863d8
Mahira Zabin <92553688+mahirazabin@users.noreply.github.com>
2022-10-04 00:24:30 -0600

    Delete src directory

--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-module CPSC_233_project_G54 {
-	requires javafx.controls;
-	requires javafx.graphics;
-	requires javafx.fxml;
-	requires java.desktop;
-	requires javafx.base;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
